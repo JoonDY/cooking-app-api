a16fd52e4e6ddc8723897ab252cf3ecb3cc9695c
Joon <daniel.joon.yi@gmail.com>
2021-09-13 12:00:42 -0700

    Fix nodemon and split db connection

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const passport = require('passport');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 const mountRoutes = require('./routes');
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || '3002';
 
 // app init
 const app = express();
@@ -23,7 +22,6 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
--- db/index.js
+++ db/index.js
@@ -1,6 +1,17 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const connection = {
+  development: new Pool(),
+  production: new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+};
+
+const db = process.env.DB_ENVIRONMENT || 'development';
+const pool = connection[db];
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
